50fd111b5bf87e19c8ada29e7377f6309c476306
Romain Caplier <romain.caplier@student-cs.fr>
2021-03-11 08:47:14 +0100

    Updated create_img and dataset_maker

--- utils/create_mnist_img.py
+++ utils/create_mnist_img.py
@@ -1,8 +1,16 @@
+import os
+import pathlib
 import pandas as pd
+
 from PIL import Image
 from sklearn.datasets import fetch_openml
-
 from tqdm import tqdm
+
+ROOT_PATH = pathlib.Path(__file__).resolve().parents[1].absolute()
+
+SAVE_PATH = os.path.join(ROOT_PATH, 'datasets', 'MNIST', 'raw')
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 print('Downloading MNIST...')
 mnist = fetch_openml('mnist_784', as_frame=True)
@@ -11,13 +19,14 @@ y = pd.DataFrame(y)
 
 list_names = []
 
+print('Creating images...')
 for i in tqdm(range(len(X))):
     im = Image.fromarray(X.iloc[i].values.reshape(28, 28))
     im = im.convert("L")
-    im.save(f"dataset/raw/mnist_784_{i}.jpeg")
+    im.save(os.path.join(SAVE_PATH, f'mnist_784_{i}.jpeg'))
     list_names.append(f"mnist_784_{i}.jpeg")
 
 y.insert(0, "Name", list_names, True)
 
-with open("dataset/raw/name_labels.csv", 'w') as f:
+with open(os.path.join(SAVE_PATH, 'name_labels.csv'), 'w+') as f:
     f.write(y.to_csv(index=None))
